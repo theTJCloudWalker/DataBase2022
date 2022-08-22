2e6c08faa959ea588b87dde91e0350eb13db9f8e
theTJCloudWalker <81422226+theTJCloudWalker@users.noreply.github.com>
2022-08-22 17:17:40 +0800

    Add files via upload

---
+++ Demo/AirlineTicketing/Service/TicketService.cs
@@ -0,0 +1,17 @@
+﻿using AirlineTicketing.Model;
+using AirlineTicketing.Dao;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirlineTicketing.Service;
+
+public interface ITicketService
+{
+    
+}
+
+public class TicketService:ITicketService
+{
+    private readonly TicketDao _ticketDao = new TicketDao();
+    
+    
+}
