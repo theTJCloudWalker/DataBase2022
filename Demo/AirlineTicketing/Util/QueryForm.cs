6e185ffe0e13a7ee4603fd466aa256f21e31ff84
theTJCloudWalker <81422226+theTJCloudWalker@users.noreply.github.com>
2022-08-22 17:16:23 +0800

    航班搜索表单

---
+++ Demo/AirlineTicketing/Util/QueryForm.cs
@@ -0,0 +1,28 @@
+﻿namespace AirlineTicketing.Util;
+
+public enum Cabin
+{
+    FIRST_CLASS,
+    BUSSINESS_CLASS,
+    ECONOMY_CLASS,
+    ALL
+}
+
+//搜索表单
+public class QueryForm
+{
+    public string destination;
+    public string depart;
+
+    //舱位
+    public Cabin cabin;
+
+    //时间区间
+    public DateTime startTime;
+    public DateTime endTime; 
+    
+    public QueryForm()
+    {
+        
+    }
+}
