b1df0b333e26ce01023247e578e8e2254c828fc2
theTJCloudWalker <81422226+theTJCloudWalker@users.noreply.github.com>
2022-08-22 17:17:16 +0800

    Add files via upload

---
+++ Demo/AirlineTicketing/Dao/TickerDao.cs
@@ -0,0 +1,15 @@
+﻿using AirlineTicketing.Model;
+
+namespace AirlineTicketing.Dao
+{
+    public interface ITickerDao
+    {
+        
+    }
+
+    public class TicketDao : Repository<Ticket>, ITickerDao
+    {
+        
+    }
+}
+
